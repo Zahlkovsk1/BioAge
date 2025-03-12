ff3cbc07f0b1d257c2616a293943dd64090e9eed
Shohjakhon Mamadaliev <shohjakhon.mamadaliev@ipratico.it>
2025-03-12 12:38:45 +0100

    Initial Commit

---
+++ BioAge/BioAgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioAgeApp.swift
+//  BioAge
+//
+//  Created by Shohjakhon Mamadaliev on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct BioAgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
